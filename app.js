311861a241de1f000ac643af8b4dde0ee5109ffd
jacopolanzoni <jacopolanzoni@gmail.com>
2019-12-26 12:46:27 +0000

    Add REST ops.

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const { MongoClient } = require('mongodb');
+const assert = require('assert');
 
 const circulationRepo = require('./repos/circulationRepo');
 const data = require('./circulation.json');
@@ -7,13 +8,73 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'circulation';
 
 async function main(){
+
   const client = new MongoClient(url, { useUnifiedTopology: true } );
   await client.connect();
-  const results = await circulationRepo.loadData(data);
-  console.log(results.insertedCount, results.ops);
-  const admin = client.db(dbName).admin();
-  console.log(await admin.serverStatus());
-  console.log(await admin.listDatabases());
+
+  try {
+    // Load data
+    const results = await circulationRepo.loadData(data);
+    assert.equal(data.length, results.insertedCount);
+
+    // Get
+    const getData = await circulationRepo.get();
+    assert.equal(data.length, getData.length);
+
+    // Get by filter
+    const filterData = await circulationRepo.get({
+      Newspaper: getData[4].Newspaper
+    });
+    assert.deepEqual(filterData[0], getData[4]);
+
+    // Get by limit
+    const limitData = await circulationRepo.get({}, 3);
+    assert.equal(limitData.length, 3);
+
+    // Get by id 
+    const id = getData[4]._id;
+    const byId = await circulationRepo.getById(id);
+    assert.deepEqual(byId, getData[4]);
+
+    // Add
+    const newItem = {
+      "Newspaper": "My paper",
+      "Daily Circulation, 2004": 1,
+      "Daily Circulation, 2013": 2,
+      "Change in Daily Circulation, 2004-2013": 100,
+      "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
+      "Pulitzer Prize Winners and Finalists, 2004-2014": 0,
+      "Pulitzer Prize Winners and Finalists, 1990-2014": 0
+    };
+    const addedItem = await circulationRepo.add(newItem);
+    assert(addedItem._id);
+    const addedItemQuery = await circulationRepo.getById(addedItem._id);
+    assert.deepEqual(addedItemQuery, newItem);
+
+    // Update
+    await circulationRepo.update(addedItem._id, {
+      "Newspaper": "My new paper",
+      "Daily Circulation, 2004": 1,
+      "Daily Circulation, 2013": 2,
+      "Change in Daily Circulation, 2004-2013": 100,
+      "Pulitzer Prize Winners and Finalists, 1990-2003": 0,
+      "Pulitzer Prize Winners and Finalists, 2004-2014": 0,
+      "Pulitzer Prize Winners and Finalists, 1990-2014": 0      
+    });
+    const newAddedItemQuery = await circulationRepo.getById(addedItem._id);
+    assert.equal(newAddedItemQuery.Newspaper, "My new paper");
+
+    // Remove
+    const removed = await circulationRepo.remove(addedItem._id);
+    assert(removed);
+    assert.equal(await circulationRepo.getById(addedItem._id), null);
+
+  } catch (error) {
+    console.log(error);
+  } finally {
+    await client.db(dbName).dropDatabase();  
+    client.close();  
+  }
 }
 
 main();
--- repos/circulationRepo.js
+++ repos/circulationRepo.js
@@ -1,9 +1,62 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient, ObjectID } = require('mongodb');
 
 function circulationRepo() {
 
   const url = 'mongodb://localhost:27017';
   const dbName = 'circulation';
+
+  function add(item) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url, { useUnifiedTopology: true });
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        const addedItem = await db.collection('newspapers').insertOne(item);
+        resolve(addedItem.ops[0]);
+      } catch(error) {
+        reject(error);
+      } finally {
+        client.close();  
+      }
+    });
+  }
+
+  function get(query, limit) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url, { useUnifiedTopology: true });
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        let items = db.collection('newspapers').find(query);
+        if (limit > 0) {
+          items = items.limit(limit);
+        }
+        resolve(await items.toArray());
+      } catch(error) {
+        reject(error);
+      } finally {
+        client.close();  
+      }
+    });
+  }
+
+  function getById(id) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url, { useUnifiedTopology: true });
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        const item = await db.collection('newspapers').findOne({
+          _id: ObjectID(id)
+        });
+        resolve(item);
+      } catch(error) {
+        reject(error);
+      } finally {
+        client.close();  
+      }
+    });
+  }
 
   function loadData(data) {
     return new Promise(async (resolve, reject) => {
@@ -13,13 +66,48 @@ function circulationRepo() {
         const db = client.db(dbName);
         results = await db.collection('newspapers').insertMany(data);
         resolve(results);
+        client.close();
       } catch (error) {
         reject(error);
       }
     });
   }
 
-  return { loadData }
+  function remove(id) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url, { useUnifiedTopology: true } );
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        removed = await db.collection('newspapers').deleteOne({
+          _id: ObjectID(id)
+        });
+        resolve(removed.deletedCount === 1);
+        client.close();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  function update(id, newItem) {
+    return new Promise(async (resolve, reject) => {
+      const client = new MongoClient(url, { useUnifiedTopology: true } );
+      try {
+        await client.connect();
+        const db = client.db(dbName);
+        updateItem = await db.collection('newspapers').findOneAndReplace({
+          _id: ObjectID(id)
+        }, newItem);
+        resolve(updateItem.value);
+        client.close();
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+
+  return { add, get, getById, loadData, remove, update }
 
 }
 
